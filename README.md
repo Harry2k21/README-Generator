bdfac9eb6469d310f872c9c405744542c5bf23c9
Harry2k21 <parker2002hp@gmail.com>
2023-05-05 20:00:08 +0100

    updated the js file

---
+++ README.md
@@ -0,0 +1,59 @@
+
+        # README Generator
+
+        Created a Console for finanicalal analysis in javascript which uses data from a multidimensional array 
+        Link - https://harry2k21.github.io/README-Generator
+        
+        ## Preview
+        
+    
+
+        ## Description 
+        
+        Generates a README for your Github Coding Projects
+        
+        ## Table of Contents
+        
+        
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [License](#license)
+        * [Features](#Features)
+        * [Badges](#Badges)
+        
+        
+        
+        ## Installation
+        
+        You can download it from my repository and clone it into your own VS code using the ssh key or download it as zip
+        
+        ## Usage
+        
+          To use the README Generator you need to open index.js in "Open Integrated Terminal" by left clicking on the file in VS code and open it in git bash and then run the command "node index.js" Then it should prompt you with inputs to fill out for the README to be Generated.
+        
+        ## License
+        
+          MIT
+        
+        ## Badges
+        
+        ![badmath](https://img.shields.io/github/languages/top/Harry2k21/README-Generator?style=plastic)
+        
+        ## Features
+        
+        Creates a proffessional and unique README based on the users input in the node terminal
+
+        ## Contributions
+
+        undefined
+
+        ## Tests
+
+        I tested this project multiple times in my own terminal and it generated a unique read me everytime
+
+        ## Questions
+
+        If you have any questions please feel free and to contact me below are my contact details
+        Email: parker2002hp@gmail.com
+        Github: Harry2k21
+        
--- index.js
+++ index.js
@@ -5,7 +5,121 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    inquirer.prompt([{
+        type: "input",
+        name: "NameOfProject",
+        question: "What the Name of the Project?",
+    },
+    {
+        type: "input",
+        name: "Description",
+        question: "Give a Brief Description of the project.",
+    },
+    {
+        type: "input",
+        name: "Installation",
+        question: "How do I install it on my system?",
+    },
+    {
+        type: "input",
+        name: "Usage",
+        question: "How can my project be used, providing a use case",
+    },
+    {
+        type: "input",
+        name: "License",
+        question: "What License is this Project Under?",
+    },
+    {
+        type: "input",
+        name: "Contributors",
+        question: "Who contributed to this Project?",
+    },
+    {
+        type: "input",
+        name: "Tests",
+        question: "What test were ran when creating this Project?",
+    },
+    {
+        type: "input",
+        name: "Email",
+        question: "Please Enter Your Email",
+    },
+    {
+        type: "input",
+        name: "Github",
+        question: "Please Enter Your Github username",
+    },
+    ]).then(function(data){
+        console.log(data.NameOfProject)
+        const template =
+        `
+        # ${data.NameOfProject}
 
+        Created a Console for finanicalal analysis in javascript which uses data from a multidimensional array 
+        Link - https://harry2k21.github.io/README-Generator
+        
+        ## Preview
+        
+    
+
+        ## Description 
+        
+        ${data.Description}
+        
+        ## Table of Contents
+        
+        
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [License](#license)
+        * [Features](#Features)
+        * [Badges](#Badges)
+        * [Contributions](#Contributions)
+        * [Tests](#Tests)
+        * [Question](#Questions)
+        
+        
+        
+        ## Installation
+        
+        ${data.Installation}
+        
+        ## Usage
+        
+        ${data.Usage}
+        
+        ## License
+        
+        ${data.License}
+        
+        ## Badges
+        
+        ![badmath](https://img.shields.io/github/languages/top/Harry2k21/README-Generator?style=plastic)
+        
+        ## Features
+        
+        Creates a proffessional and unique README based on the users input in the node terminal
+
+        ## Contributions
+
+        ${data.Contributors}
+
+        ## Tests
+
+        ${data.Tests}
+
+        ## Questions
+
+        If you have any questions please feel free and to contact me below are my contact details
+        Email: ${data.Email}
+        Github: ${data.Github}
+        `
+    
+        fs.writeFile("README.md", template, function(err){
+            err ? console.error(err) : console.log('Successfully logged!')
+        })
+    })
 ];
 
 // function to write README file
